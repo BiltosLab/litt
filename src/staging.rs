a50dcf306ae41a1c163e2330e5f50195d27085ab
BiltosLab <mrlaith44@gmail.com>
2024-01-13 10:31:08 +0300

    Basic ignoring added as Vec<String> later we add .littignore

--- src/staging.rs
+++ src/staging.rs
@@ -1,17 +1,22 @@
-use std::{fs,fs::DirBuilder,fs::File,io::Write,io::Read};
+use std::{fs,fs::DirBuilder,fs::File,io::Write,io::Read, borrow::{Borrow, BorrowMut}};
 use sha2::{Sha256, Digest};
 
 
 
 pub fn add() { //template for add func
-    let list = scanfile("./");
+    let mut ignorelist:Vec<String> = Vec::new();
+    ignorelist.push(".git".to_string());
+    ignorelist.push("target".to_string());
+    // Add logic to parse a new file called 
+    //.littignore later that will push everyline to this Vec and ignore anything in that list
+    let list = scanfile("./",&ignorelist);
     let mut f  = File::create("./.litt/config.txt").unwrap();
     
 
     for l in list{
         println!("{}",l);
         f.write_all(l.as_bytes()).unwrap();
-        f.write_all(b"\t").unwrap();
+        f.write_all(b"\n").unwrap();
         f.write_all(computehash(&l).as_bytes()).unwrap();
         f.write_all(b"\n").unwrap();
     }
@@ -21,16 +26,16 @@ pub fn add() { //template for add func
 }
 
 
-pub fn scanfile(realpath:&str) -> Vec<String> {
+pub fn scanfile(realpath:&str,ignore:&Vec<String>) -> Vec<String> {
     let mut filelist:Vec<String> = Vec::new();
-
     if let Ok(dirf) = fs::read_dir(realpath)
     {
         for path in dirf{
             if let Ok(path) = path {
                 if let Ok(metta) = path.metadata(){ 
                     if metta.is_dir(){
-                        filelist.extend(scanfile(&path.path().to_string_lossy()));
+                        if ignore.contains(path.file_name().to_string_lossy().to_string().borrow_mut()) {continue;}
+                        filelist.extend(scanfile(&path.path().to_string_lossy(),ignore));
                     }
                     else if metta.is_file() {
                         //println!("{:?}",path.path().to_str().unwrap());
