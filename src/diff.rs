9ed91057955064172ea7feb10faee0ca0890fc6b
BiltosLab <mrlaith44@gmail.com>
2024-01-17 18:29:14 +0300

    colorize the - with red and + with green

--- src/diff.rs
+++ src/diff.rs
@@ -11,14 +11,14 @@ pub fn find_diff_lines(file1: Vec<String>, file2: Vec<String>) -> Vec<String> {
                 //differences.push(format!("  {}", line1));
             }
             (Some(line1), Some(line2)) => {
-                differences.push(format!("- {}", line1));
-                differences.push(format!("+ {}", line2));
+                differences.push(format!("- {}", line1.red()));
+                differences.push(format!("+ {}", line2.green()));
             }
             (Some(line1), None) => {
-                differences.push(format!("- {}", line1));
+                differences.push(format!("- {}", line1.red()));
             }
             (None, Some(line2)) => {
-                differences.push(format!("+ {}", line2));
+                differences.push(format!("+ {}", line2.green()));
             }
             _ => unreachable!(), // This case should not happen due to max_lines
         }
