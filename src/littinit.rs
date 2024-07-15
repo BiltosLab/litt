65996207991a095ec98729a928391d4fdfa95d36
Biltoslab <mrlaith44@gmail.com>
2024-07-15 10:48:29 +0300

    Added Check if repo exists in init

--- src/littinit.rs
+++ src/littinit.rs
@@ -3,6 +3,13 @@ use std::{fs,fs::DirBuilder,fs::File,io::Write};
 use crate::filestuff::*;
 
 pub fn init() { // TODO! : Implement an init func aka create files and dirs and setup something idk
+    if !file_exists(".litt") {
+        println!("fatal: not a litt repository ");
+        return;
+    }
+
+
+
     let filestruct = ["./.litt","./.litt/branches","./.litt/hooks","./.litt/info","./.litt/refs","./.litt/refs/heads","./.litt/refs/tags","./.litt/objects","./.litt/objects/pack","./.litt/objects/info",]; // we can add whatever folders we need to create at init time.
     let files = ["./.litt/info/exclude","./.litt/description","./.litt/HEAD","./.littignore","./.litt/index"];
     println!("Init");
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,65 @@
-use std::{fs,env, process::exit, io};
-use colored::*;
 use crate::{filestuff::*, staging::catfile};
-mod littinit;
-mod staging;
-mod filestuff;
-mod diff;
+use colored::*;
+use std::{env, fs, io, process::exit};
 mod commits;
+mod diff;
+mod filestuff;
+mod littinit;
 mod parsingops;
-// figure out how to use env variables to store email,name of the commiter much easier than the other one i was thinking about 
-// which was create a file in .config/litt and store info there idk tbh 
+mod staging;
+// figure out how to use env variables to store email,name of the commiter much easier than the other one i was thinking about
+// which was create a file in .config/litt and store info there idk tbh
 fn main() -> Result<(), io::Error> {
-    let args:Vec<String> = env::args().collect();
-    if args.len() < 2 {helpcom();exit(1);}
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        helpcom();
+        exit(1);
+    }
     let cmd = &args[1];
     match cmd.as_str() {
         "init" => littinit::init(),
-        "add" => { if args.len() < 3 {println!("ERROR! No args provided for add.");exit(1);}
+        "add" => {
+            if args.len() < 3 {
+                println!("ERROR! No args provided for add.");
+                exit(1);
+            }
             staging::add(args.into_iter().skip(2).collect());
-        },
+        }
         "commit" => commits::commit(),
         "status" => status()?,
         "log" => log(),
-        "cat-file" => { if args.len() < 3 {println!("ERROR! No args provided for cat-file.");exit(1);}
-           if args[2].len() >= 7 {catfile(&args[2]);}
-        },
+        "cat-file" => {
+            if args.len() < 3 {
+                println!("ERROR! No args provided for cat-file.");
+                exit(1);
+            }
+            if args[2].len() >= 7 {
+                catfile(&args[2]);
+            }
+        }
         _ => println!("Unknown command: {}", cmd),
     }
     println!("DONE!");
     exit(0);
 }
 
-
-
-
-
-fn status() ->Result<(), io::Error>  { //template for status func
-    let original_lines = filetostring("./src/main.rs")?; // Just a note here : This original lines Vec<String> will get fetched from the database file using the commit number and our currect files will get compared to it 
-    let modified_lines= filetostring("./src/main1.rs")?; //adbasdds
+fn status() -> Result<(), io::Error> {
+    //template for status func
+    let original_lines = filetostring("./src/main.rs")?; // Just a note here : This original lines Vec<String> will get fetched from the database file using the commit number and our currect files will get compared to it
+    let modified_lines = filetostring("./src/main1.rs")?; //adbasdds
     let linediff = diff::find_diff_lines(original_lines, modified_lines);
     println!("Modified Lines Test:\n{}", linediff.join("\n"));
     Ok(())
-    
 }
 
-fn log() {  //template for log func
+fn log() {
+    //template for log func
     println!("Commit history:");
     parsingops::indexparser();
     //println!("{:?}",parsingops::indexparser());
 }
 
-
-
-fn helpcom(){
+fn helpcom() {
     scanfiles_and_ignore(".");
     println!("Litt Usage:\nlitt <first arg> <second arg> <third arg>\nEX: litt add . OR litt add file1.c file2.c\n");
 }
