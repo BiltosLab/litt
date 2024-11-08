e0b1f25b13d2e21954364d4d62cf5a8150f7df52
Biltoslab <mrlaith44@gmail.com>
2024-11-08 07:24:18 +0300

    Implemented new (fixed) selective file adding.

--- src/staging.rs
+++ src/staging.rs
@@ -1,41 +1,35 @@
-use crate::fileops::{blob, compress_files_in_parallel};
+use crate::fileops::compress_files_in_parallel;
 use crate::scan_for_staging;
-use crate::{file_exists, fileops::scanfiles_and_ignoremt};
+use crate::file_exists;
 use colored::Colorize;
-use std::borrow::Borrow;
 
-pub fn add(args:Vec<String>) {
-    if !file_exists("./.litt") { // I think this will suffice
-        println!("{}: not a litt repository ","fatal".red());
+pub fn add(args: Vec<String>) {
+    if !file_exists("./.litt") {
+        // I think this will suffice
+        println!("{}: not a litt repository ", "fatal".red());
         return;
     }
     if args[0] == "." {
-        // let file_list = scanfiles_and_ignoremt(".",true);
-        let (file_list,hashmp) = scan_for_staging(".",true);
-        if !file_list.is_empty(){
-            let _result= compress_files_in_parallel(file_list,hashmp).expect("TODO: panic staging.rs L16");
+        let (file_list, hashmp) = scan_for_staging(".", true);
+        if !file_list.is_empty() {
+            let _result =
+                compress_files_in_parallel(file_list, hashmp).expect("TODO: panic staging.rs");
+        } else {
+            println!("{}", "No changes to add to staging area".bright_cyan());
         }
-        else {
-            println!("{}","No changes to add to staging area".bright_cyan());
-        }
-        // We need to change this to only compress/add to staging modified files ONLY.
-
-
-    }
-    else { // IDK if this needs to be multithreaded too because the user prob will enter the names of like 5 or 6 files max ?
-        for file in args { // DO NOT USE THIS NOW! // TODO
-            if file_exists(&file) {
-                println!("{}", "Incomplete Feature use the . instead!!".red());
-                blob(&file); // This needs to be changed to add file to index
-                println!("File compressed {} :",file);}
-            else {
-                println!("'{}' did not match any file",file.to_string().red());
+        // We need to change this to only compress/add to staging modified files ONLY.[DONE]
+    } else {
+        println!("ARGS DEBUG : {:#?}", args);
+        let mut all_files_ok: bool = true;
+        for file in &args {
+            if !file_exists(&file) {
+                println!("'{}' did not match any file", file.to_string().red());
+                all_files_ok = false;
             }
+        }
+        if all_files_ok {
+            let hashmp = scan_for_staging(".", true);
+            let _ = compress_files_in_parallel(args, hashmp.1).expect("TODO: panic staging.rs");
         }
     }
 }
-
-
-
-
-
